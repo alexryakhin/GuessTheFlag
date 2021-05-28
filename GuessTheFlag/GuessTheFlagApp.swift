38bd9978d813c75d9910b9d953ca7e85d7a37f7c
Alexander Ryakhin <bonney977@gmail.com>
2021-05-28 09:00:28 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alexander Bonney on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
